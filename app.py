5708b619b192dea37c835fcfb3acb43a5eea79c1
taku-743336 <tcfield0402@gmail.com>
2025-04-04 17:35:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+import os
 import openai
 
 app = Flask(__name__)
